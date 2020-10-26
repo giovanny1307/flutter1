d7edc00878817417c86c7144447130590f12545c
giovanny1307 <giovanny1307@gmail.com>
2020-10-26 10:11:39 -0500

    first commit w

---
+++ flutter1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// este es mi core widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
