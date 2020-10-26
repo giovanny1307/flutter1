be3d8cbca87e04e8602d55225568f318a68a6b63
giovanny1307 <giovanny1307@gmail.com>
2020-10-26 10:55:09 -0500

    haciendo preguntas

--- flutter1/lib/main.dart
+++ flutter1/lib/main.dart
@@ -4,13 +4,54 @@ void main() {
   runApp(MyApp());
 }
 
-// este es mi core widget
+// este es mi core widget y funciona como la raiz del proyecto
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
+    void answerQuestion()  {
+        print('Holi holi');
+      }
+
     return MaterialApp(
-      home: Text('Hello!'),
+      
+
+      // un scaffold funciona para dar una estructura base
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter 1'),
+        ),
+        // en el body ud puede meter widgets que dan layou structure como colum
+        // hay widgets visibles e invisibles, los invisibles dan layout structure
+        body: (Column(
+          children: <Widget>[
+            Text('Question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              // asi se declara una func anonima
+              onPressed: () => print('Presiono el 1'),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              //** esto es un pointer a answer question
+              //** lo cual es muy diferente a llamar answerQuestion ya que 
+              //** eso me corre la funcion y quiero retornar la funcion 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              // otra forma de func anonima    
+              onPressed:() {
+                print('Presiono la 3');
+              },
+            )
+          ],
+        )),
+      ),
     );
   }
 }
